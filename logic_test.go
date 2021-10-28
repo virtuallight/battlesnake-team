354e665c628f6e99dd799e2212569723ede3a96b
Sławek Ehlert <slawomir.ehlert@gmail.com>
2021-10-28 09:18:08 +0200

    Add convertFromGBE function
    Add a reversed version of convertToGBE function
    and add a test.
    
    Should be useful for printing and debugging game boards.
    
    Also, extracted test data var to a global var.

--- logic_test.go
+++ logic_test.go
@@ -6,6 +6,51 @@ import (
 	"github.com/matryer/is"
 )
 
+var testData = []struct {
+	state                 GameState
+	expectedExtendedBoard GameBoardExtended
+}{
+	{
+		state: GameState{
+			Board: Board{
+				Height: 2,
+				Width:  3,
+			},
+		},
+		expectedExtendedBoard: convertToGBE(VisualGameBoardExtended{
+			{Tile{}, Tile{}, Tile{}},
+			{Tile{}, Tile{}, Tile{}},
+		}),
+	},
+	{
+		state: GameState{
+			Board: Board{
+				Height: 3,
+				Width:  4,
+				Snakes: []Battlesnake{
+					{
+						Head: Coord{X: 2, Y: 0},
+						Body: []Coord{{X: 2, Y: 0}, {X: 1, Y: 0}, {X: 0, Y: 0}},
+					},
+					{
+						Head: Coord{X: 2, Y: 1},
+						Body: []Coord{{X: 2, Y: 1}},
+					},
+				},
+				Food: []Coord{
+					{X: 3, Y: 2},
+					{X: 3, Y: 0},
+				},
+			},
+		},
+		expectedExtendedBoard: convertToGBE(VisualGameBoardExtended{
+			{Tile{}, Tile{}, Tile{}, Tile{Food}},
+			{Tile{}, Tile{}, Tile{Head}, Tile{}},
+			{Tile{Body}, Tile{Body}, Tile{Head}, Tile{Food}},
+		}),
+	},
+}
+
 func TestNeckAvoidance(t *testing.T) {
 	// Arrange
 	me := Battlesnake{
@@ -34,56 +79,21 @@ func TestNeckAvoidance(t *testing.T) {
 
 func TestCreateGameBoardExtended(t *testing.T) {
 
-	testData := []struct {
-		state                 GameState
-		expectedExtendedBoard GameBoardExtended
-	}{
-		{
-			state: GameState{
-				Board: Board{
-					Height: 2,
-					Width:  3,
-				},
-			},
-			expectedExtendedBoard: convertToGBE(VisualGameBoardExtended{
-				{Tile{}, Tile{}, Tile{}},
-				{Tile{}, Tile{}, Tile{}},
-			}),
-		},
-		{
-			state: GameState{
-				Board: Board{
-					Height: 3,
-					Width:  4,
-					Snakes: []Battlesnake{
-						{
-							Head: Coord{X: 2, Y: 0},
-							Body: []Coord{{X: 2, Y: 0}, {X: 1, Y: 0}, {X: 0, Y: 0}},
-						},
-						{
-							Head: Coord{X: 2, Y: 1},
-							Body: []Coord{{X: 2, Y: 1}},
-						},
-					},
-					Food: []Coord{
-						{X: 3, Y: 2},
-						{X: 3, Y: 0},
-					},
-				},
-			},
-			expectedExtendedBoard: convertToGBE(VisualGameBoardExtended{
-				{Tile{}, Tile{}, Tile{}, Tile{Food}},
-				{Tile{}, Tile{}, Tile{Head}, Tile{}},
-				{Tile{Body}, Tile{Body}, Tile{Head}, Tile{Food}},
-			}),
-		},
-	}
-
 	is := is.New(t)
 
 	for _, data := range testData {
 		gameBoardExtended := createGameBoardExtended(data.state)
 		is.Equal(gameBoardExtended, data.expectedExtendedBoard) // The result gameBoardExtended isn't what we expected it to be
+	}
+}
+
+func TestVisualConvert(t *testing.T) {
+	is := is.New(t)
+
+	for _, data := range testData {
+		visual := convertFromGBE(data.expectedExtendedBoard)
+		backGBE := convertToGBE(visual)
+		is.Equal(backGBE, data.expectedExtendedBoard) // Converting extended game board to visual representation and back should give back the same board.
 	}
 }
 
@@ -93,6 +103,8 @@ type VisualGameBoardExtended [][]Tile
 // (where value at (0, 0) is at the bottom left corner)
 // and converts it, so that the field at (0, 0)
 // is actually represented on GameBoardExtended at the [0][0] index and so on.
+// This function is useful for taking a visual representation of the board
+// and converting it to something that logic understands.
 func convertToGBE(visual VisualGameBoardExtended) GameBoardExtended {
 	gbe := GameBoardExtended{}
 	rows := len(visual[0]) // height
@@ -107,8 +119,23 @@ func convertToGBE(visual VisualGameBoardExtended) GameBoardExtended {
 	return gbe
 }
 
-// func convertFromGBE(gbe GameBoardExtended) VisualGameBoardExtended {
-// 	var tiles [][]Tile
+// convertFromGBE takes an extended game board argument
+// (where value at (0, 0) is at index [0][0])
+// and converts it, so that the field at (0, 0)
+// is at the bottom left corner.
+// This function is useful for printing out GameBoardExtended values
+// for debugging.
+func convertFromGBE(gbe GameBoardExtended) VisualGameBoardExtended {
+	visual := VisualGameBoardExtended{}
 
-// 	return tiles
-// }
+	rows := len(gbe)       // height
+	columns := len(gbe[0]) // width
+	for x := 0; x < columns; x++ {
+		line := []Tile{}
+		for y := 0; y < rows; y++ {
+			line = append(line, gbe[y][columns-x-1])
+		}
+		visual = append(visual, line)
+	}
+	return visual
+}
